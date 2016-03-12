85bafaeb269ea359ee80346c25afa440167e1bb0
Rahn Kim <apatosaurus@rahnkim.com>
2016-03-13 00:42:47 +1100

    Timeline service batch upload feature.

--- timeline-service/app.py
+++ timeline-service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import base64
+import cgi
 import json
 import urllib
 
@@ -34,16 +35,25 @@ class BioHandler(webapp2.RequestHandler):
     self.response.write("{0}({1})".format(callback, json.dumps(bios)))
 
   def post(self):
-    bio = Bio()
-    bio.name = self.request.get("name")
-    bio.birth_year = int(self.request.get("birth_year"))
-    death_year_raw_input = self.request.get("death_year")
-    if death_year_raw_input:
-      bio.death_year = int(death_year_raw_input)
-    bio.blurb = self.request.get("blurb")
-    bio.picture = base64.b64encode(self.request.get("picture"))
-    bio.put()
-    self.redirect('/new')
+    try:
+      bio = Bio()
+      bio.name = self.request.POST.get("name")
+      bio.birth_year = int(self.request.POST.get("birth_year"))
+      death_year_raw_input = self.request.POST.get("death_year")
+      if death_year_raw_input and death_year_raw_input != "undefined":
+        bio.death_year = int(death_year_raw_input)
+      bio.blurb = self.request.POST.get("blurb")
+      print self.request.POST.get("picture")
+      bio.picture = base64.b64encode(self.request.POST.get("picture").file.read())
+      bio.put()
+      self.response.write("Successfully added bio for {0}.".format(bio.name))
+      if not self.request.POST.get("batch"):
+        self.redirect('/new')
+    except Exception as inst:
+      self.response.write(inst)
+
+  def delete(self):
+    db.delete(Entry.all(keys_only=True))
 
 
 class BioSubmissionHandler(webapp2.RequestHandler):
@@ -52,30 +62,34 @@ class BioSubmissionHandler(webapp2.RequestHandler):
     user = users.get_current_user()
     if user:
       if users.is_current_user_admin():
-        self.response.out.write('<h1>New bio entry</h1>')
         self.response.out.write("""
 		  <form action="/"
 				enctype="multipart/form-data"
 				method="post">
-			<div><label>Name:</label></div>
-			<div>
-			  <textarea name="name" rows="1" cols="60"></textarea>
-			</div>
-			<div><label>Birth Year:</label></div>
-			<div>
-			  <textarea name="birth_year" rows="1" cols="30"></textarea>
-			</div>
-			<div><label>Death Year:</label></div>
-			<div>
-			  <textarea name="death_year" rows="1" cols="30"></textarea>
-			</div>
-			<div><label>Blurb:</label></div>
-			<div>
-			  <textarea name="blurb" rows="6" cols="60"></textarea>
-			</div>
-			<div><label>Picture:</label></div>
-			<div><input type="file" name="picture"/></div>
-			<div><input type="submit" value="Upload new bio"></div>
+			<fieldset>
+			  <h1>Load Single Bio</h1>
+			  <div><label>Name*:</label></div>
+			  <div>
+			    <textarea name="name" rows="1" cols="60"></textarea>
+			  </div>
+			  <div><label>Birth Year*:</label></div>
+			  <div>
+			    <textarea name="birth_year" rows="1" cols="30"></textarea>
+			  </div>
+			  <div><label>Death Year:</label></div>
+			  <div>
+			    <textarea name="death_year" rows="1" cols="30"></textarea>
+			  </div>
+			  <div><label>Blurb*:</label></div>
+			  <div>
+			    <textarea name="blurb" rows="6" cols="60"></textarea>
+			  </div>
+			  <div><label>Picture*:</label></div>
+			  <div><input type="file" name="picture"/></div>
+			  <div style="margin-top: 15px">
+			    <input type="submit" value="UPLOAD!">
+			  </div>
+			</fieldset>
 		  </form>""")
       self.response.out.write('<p><a href="%s">Sign out</a>.</p></body></html>' %
                                   users.create_logout_url('/new'))
@@ -83,9 +97,25 @@ class BioSubmissionHandler(webapp2.RequestHandler):
       self.response.out.write('<p><a href="%s">Sign in</a>.</p></body></html>' %
                                   users.create_login_url('/new'))
 
+class BioBatchHandler(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if user:
+      if users.is_current_user_admin():
+        self.response.out.write(open("batch.html").read())
+      else:
+        self.response.out.write(
+            '<html><body><p><a href="%s">Sign out</a>.</p></body></html>' %
+                users.create_logout_url('/new'))
+    else:
+      self.response.out.write(
+          '<html><body><p><a href="%s">Sign in</a>.</p></body></html>' %
+              users.create_login_url('/new'))
+
 
 app = webapp2.WSGIApplication(
     [
-        ("/", BioHandler), ("/new", BioSubmissionHandler)
+        ("/", BioHandler), ("/new", BioSubmissionHandler),
+        ("/newbatch", BioBatchHandler)
     ],
     debug=True)
