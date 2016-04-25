e821537d280f36db892df4950800e8a9e12bb241
shainanigans <me@shainakoval.com>
2016-04-25 18:08:48 +1000

    Add additional styles, aria attr and scroll to functionality

--- main/src/js/main.js
+++ main/src/js/main.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function($){
-
+	// On clicking the "to top" button, jump/scroll to the top of page.
+	$('#to-top').click(function() {
+	    $('html, body').animate({
+	        scrollTop: $('#top').offset().top
+	    }, 500);
+	});
 });
